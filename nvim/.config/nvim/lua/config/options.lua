04619e47f83e3f21e143f0999d4217b6baed25ae
Caetano Cabral <caetano29cabral@gmail.com>
2026-07-12 23:24:42 -0300

    feat(nvim): LazyVim Python setup + common QoL extras
    Enable lang.python (pyright, ruff, debugpy, venv-selector already
    bundled), plus json/yaml/markdown extras, dial and dot editor extras.
    Override pyright typeCheckingMode to basic. Add relativenumber,
    scrolloff and nowrap to options.lua.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.relativenumber = true -- relative line numbers, easier motions (5j, 3dd)
+opt.scrolloff = 8 -- keep context visible when scrolling
+opt.wrap = false -- no soft-wrap on long lines
